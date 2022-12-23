bab090b447b9e7f5789d350ba37a4438516262e3
Baek <tjdgus5761@gmail.com>
2022-12-23 13:40:33 +0900

    소스 분할 및 인디케이터 구현

---
+++ src/screens/default/home.tsx
@@ -0,0 +1,27 @@
+import Reack from 'react';
+import { Button, View, Text,Pressable,StyleSheet,TouchableOpacity,TextInput } from 'react-native';
+
+const Q_Home = ({ navigation, route }) => {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>
+              환영합니다, 하해호님!                
+              <Text>
+                  설문을 시작
+              </Text>
+          </Text>
+          <Button
+              title=" 시작하기 "
+              color="#F47100"
+              onPress={() => navigation.navigate('Q_Default_1')}
+          />
+          <Button
+              title=" 나중에 할게요 "
+              color="#F47100"
+              onPress={() => alert('to the next')}
+    />
+      </View>
+  );
+};
+
+export default Q_Home;
---
+++ src/screens/default/q_default_2.tsx
@@ -0,0 +1,15 @@
+import {View,Text} from 'react-native';
+import MyUpBar_1 from '../../upbar/UpBar_1';
+
+const Q_Default_2 = ({ navigation, route }) => {
+  return (
+      <>
+      <MyUpBar_1/>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>            
+          <Text>ggg</Text>
+      </View>
+      </>
+  );
+};
+
+export default Q_Default_2;
