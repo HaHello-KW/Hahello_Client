db0eb02a19067a0ef608c139f0554bb0b0075bb0
BaekSeongHyeon <81316185+BaekSeongHyeon@users.noreply.github.com>
2022-12-28 17:59:42 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,120 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  Button,
   View,
+  Text,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import {Box, HStack} from '@react-native-material/core';
+import MyUpBar_0 from './src/upbar/UpBar_0';
+import MyUpBar_1 from './src/upbar/UpBar_1';
+import MyUpBar_2 from './src/upbar/UpBar_2';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Q_Home from './src/screens/default/home';
+import Q_Default_1 from './src/screens/default/q_default_1';
+import Q_Default_2 from './src/screens/default/q_default_2';
+import Q_Default_2and3 from './src/screens/default/q_default_2and3';
+import Q_Default_2_1 from './src/screens/default/q_default_2_1';
+import UserA from './src/screens/userA/test';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Home"
+          component={Q_Home}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Q_Default_1"
+          component={Q_Default_1}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Q_Default_2"
+          component={Q_Default_2}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Test"
+          component={Q_Default_2and3}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Q_Default_2_1"
+          component={Q_Default_2_1}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="UserA"
+          component={UserA}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-//안녕
 export default App;
