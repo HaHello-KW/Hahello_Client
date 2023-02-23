d3362f4e14702527358a36e6e7df5b27ead1ab45
parkjihong <hary0625@naver.com>
2023-02-23 19:52:34 +0900

    typepage구성 및 스타일링 수정

--- src/components/typePage.tsx
+++ src/components/typePage.tsx
@@ -10,7 +10,11 @@ import {
 import {defaultPageModel} from '../models/defaultPageModel';
 import { TypePageModel } from '../models/typePageModel';
 import {defaultPageStyles} from '../styles/defaultPageStyles';
+import { UserStyle } from '../styles/typePageStyles';
 import PType from './dateConverter';
+//for test
+import UserImg from './userImg';
+import UserImgHQ from './userImgHQ';
 
 type typePageProps = {
     pageContents: TypePageModel;
@@ -47,12 +51,12 @@ function TypePage({pageContents}:typePageProps){
             {/* 여기에 넣는게 맞나? 아니면 screen의 survey에?  */}
             {/* <MyUpBar level={pageContents.pgLevel} /> */}
             {/* <GobackButton onPress={() => navigation.pop()} /> */}
-            <View style={[defaultPageStyles.container_bs_q]}>
-              <Text style={[defaultPageStyles.txt]}>
+            <View style={[UserStyle.container_bs_q]}>
+              <Text style={[UserStyle.txt]}>
                 {pageContents.questionTxt}
               </Text>
             </View>
-            <View style={[defaultPageStyles.container_bs_c]}>
+            <View style={[UserStyle.container_bs_c]}>
               {pageContents.selectionTxt.map(function (value: any, index: number) {
                 return (
                   //왜 가려져서 나오지?? scrollview의 문제인가? 스타일링 관련해서 알아볼 것
@@ -95,21 +99,21 @@ function TypePage({pageContents}:typePageProps){
         return (
           <>
             {/* <MyUpBar level={pageContents.pgLevel} /> */}
-            <View style={[defaultPageStyles.container_tlp_q]}>
-              <View style={styles.container_tlp_line}>
+            <View style={[UserStyle.container_tlp_q]}>
+              <View style={UserStyle.container_tlp_line}>
                 <PType Type_of_Picker={pageContents.firstPickerType}></PType>
                 <Text style={styles.tlp_txt}>{pageContents.firstlineTxt}</Text>
               </View>
-              <View style={styles.container_tlp_line}>
+              <View style={UserStyle.container_tlp_line}>
                 <PType Type_of_Picker={pageContents.secondPickerType} />
                 <Text style={styles.tlp_txt}>{pageContents.secondlineTxt}</Text>
               </View>
-              <View style={styles.container_tlp_line}>
+              <View style={UserStyle.container_tlp_line}>
                 <PType Type_of_Picker={pageContents.thirdPickerType} />
                 <Text style={styles.tlp_txt}>{pageContents.thirdlineTxt}</Text>
               </View>
             </View>
-            <View style={[defaultPageStyles.container_tlp_c]} />
+            <View style={[UserStyle.container_tlp_c]} />
             {/* <View style={[defaultPageStyles.container_next]}>
               <NextButton destination={pageContents.nextpage} disabled={false} />
               <Text>hi hello this is test</Text>
@@ -123,7 +127,34 @@ function TypePage({pageContents}:typePageProps){
       ){
         return(
             <>
-                
+                {}
+                <View style= {UserStyle.container_slp_q}>
+                  <View style={UserStyle.container3_1}>
+                    <Text style={UserStyle.blacktxt}>
+                      {pageContents.firstlineTxt}
+                    </Text>
+                    <PType Type_of_Picker={pageContents.firstPickerType}></PType>
+                  </View>
+                  <View style={UserStyle.container3_2}>
+                    <Text style={UserStyle.blacktxt}>
+                      {pageContents.secondlineTxt}
+                    </Text>
+                    <Text style={[UserStyle.descriptionGray]}>
+                      보충보충
+                    </Text>
+                    <PType Type_of_Picker={pageContents.secondPickerType}></PType>
+                  </View>
+                  <View style={UserStyle.container3_3}>
+                    <Text style={UserStyle.blacktxt}>
+                      {pageContents.thirdlineTxt}
+                    </Text>
+                    <Text style={[UserStyle.descriptionGray]}>
+                      보충보충
+                    </Text>
+                    <PType Type_of_Picker={pageContents.thirdPickerType}></PType>
+                  </View>
+                </View>
+                {}
             </>
         )
       }
@@ -133,7 +164,6 @@ function TypePage({pageContents}:typePageProps){
       ){
         return(
             <>
-            
             </>
         )
       }
@@ -172,6 +202,14 @@ const styles = StyleSheet.create({
       backgroundColor: '#ffffff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    
+    blacktxt: {
+      fontWeight: '500',
+      fontSize: 16,
+      lineHeight: 23,
+      color: '#000000',
+      textAlign: 'center',
     },
   });
 
--- src/styles/typePageStyles.tsx
+++ src/styles/typePageStyles.tsx
@@ -3,174 +3,39 @@ import {StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export const UserStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    // width: '100%',
-    // height: 80,
-    backgroundColor: '#ffffff',
-  },
-  //
-  //질문 스타일링
-  //container0_1, 0_1_n -> 3줄 질문 스타일링
-  container0_1: {
-    flex: 2.5,
-    // width: 240,
-    // height: 140,
+  
+  //sixline_picker의 스타일링
+  container_slp_q: {
+    flex: 7,
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  blacktxt: {
+    fontWeight: '500',
+    fontSize: 16,
+    lineHeight: 23,
+    color: '#000000',
     textAlign: 'center',
-    // lineHeight: 34.75,
   },
-  container0_1_1: {
-    // flex: 1,
-    flexDirection: 'row',
-    // width: 240,
-    // height: 140,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    lineHeight: 34.75,
-  },
-  container0_1_2: {
-    // flex: 1,
-    flexDirection: 'row',
-    // width: 240,
-    // height: 140,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    lineHeight: 34.75,
-  },
-  container0_1_3: {
-    // flex: 1,
-    flexDirection: 'row',
-    // width: 240,
-    // height: 140,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    lineHeight: 34.75,
-  },
-  //한줄 질문 스타일링 && 선택지박스 2개 이하
-  container0_2: {
-    flex: 1.5,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  //한줄 질문 스타일링 && 선택지박스 3개 이상
-  container0_3: {
-    flex: 1.2,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  //
-  //선택지박스 스타일링
-  container1_1: {
-    flex: 3.5,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  //2개 이하
-  container1_2: {
-    flex: 4.5,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  //3개 이상
-  container1_3: {
-    flex: 4.8,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  //
-  //다음버튼 스타일링
-  container2: {
-    // position: 'absolute',
-    // bottom: 0,
-    // height: 90,
-    flex: 1.5,
-    width: '100%',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-  },
-  //
-  //생리주기 생리기간 스타일링 - default 예상
   container3_1: {
-    flex: 1.5,
-    // flexDirection: 'row'
+    flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
   container3_2: {
-    flex: 2,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  container3_3: {
-    flex: 2.5,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  //type result page container
-  containerR_0: {
     flex: 1,
     backgroundColor: '#ffffff',
-  },
-  containerR_1: {
-    flex: 4.5,
-    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  containerR_2: {
-    flex: 9,
+  container3_3: {
+    flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
-  containerR_3: {
-    flex: 5,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  //one? three?? 결국 같은거같은데 이거 수정 ㄱㄱ
-  onelinetxt: {
-    fontSize: 24,
-    fontWeight: '400',
-    color: '#000000',
-    textAlign: 'center',
-    lineHeight: 34.75,
-  },
-  threelinetxt: {
-    fontSize: 24,
-    fontWeight: '400',
-    color: '#000000',
-    textAlign: 'center',
-    lineHeight: 34.75,
-  },
-  //생리주기 보충설명 스타일링
-  description: {
-    fontSize: 12,
-    fontWeight: '400',
-    color: '#6F6F6F',
-    lineHeight: 20,
-    width: 300,
-    height: 55,
-  },
-  //보충설명 스타일링 gray 버전
   descriptionGray: {
     marginTop: 8,
     marginBottom: 8,
@@ -181,6 +46,52 @@ export const UserStyle = StyleSheet.create({
     backgroundColor: '#E4E4E4',
     alignSelf: 'center',
     textAlign: 'center',
+  },
+  //Threeline_picker의 스타일링
+  container_tlp_q: {
+    flex: 5,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container_tlp_line: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container_tlp_c: {
+    flex: 2,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  tlp_txt: {
+    fontSize: 24,
+    fontWeight: '400',
+    color: '#000000',
+    textAlign: 'center',
+  },
+  //buttonselector의 스타일링
+  container_bs_q: {
+    flex: 2.5,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container_bs_c: {
+    flex: 4.5,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  txt: {
+    fontSize: 24,
+    fontWeight: '400',
+    color: '#000000',
+    marginTop: 20,
+    textAlign: 'center',
+    lineHeight: 34.75,
   },
 });
 
