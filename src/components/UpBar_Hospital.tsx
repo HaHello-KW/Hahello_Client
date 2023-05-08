af61c7f434ac3ce39115dfeac1bbc894bce31412
parkjihong <hary0625@naver.com>
2023-05-08 19:47:53 +0900

    병원찾기 페이지
    1차구현

---
+++ src/components/UpBar_Hospital.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Box, HStack} from '@react-native-material/core';
+import {ImageBackground} from 'react-native';
+import homebg from '../../assets/images/home_start.png';
+import { Text } from 'react-native-paper';
+
+const UpBar_Hospital = ({text}) => {
+  return (
+    <Box w="100%" h="8%" style={{backgroundColor: '#FFFFFF'}}>
+      <Text style={{
+        left: 105,
+        top: 25,
+        width: 190,
+        height: 24,
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: '400',
+        color: '#000000',
+      }}>
+      {text}
+      </Text>
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  textInput: {
+    fontSize: 16,
+    color: '#000000',
+    height: 50,
+    width: 300,
+    //borderColor: '#000000',
+    //borderWidth: 1,
+    //borderRadius: 12,
+    padding: 10,
+  },
+  boxline: {
+    marginTop: '15%',
+    marginBottom: '10%',
+    marginRight: '3%',
+    marginLeft: '3%',
+  },
+  boxlineDone: {
+    backgroundColor: '#F47100',
+  },
+  boxlineYet: {
+    backgroundColor: '#FFFFFF',
+  },
+  buttonSelection: {
+    backgroundColor: '#F2F2F2',
+    width: 300,
+    height: 40,
+  },
+  buttonNext: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: '#F47100',
+    width: 300,
+    height: 40,
+  },
+});
+
+export default UpBar_Hospital;
---
+++ src/screens/contents/hospital_map.tsx
@@ -0,0 +1,137 @@
+import {
+    View,
+    Text,
+    Button,
+    Image,
+    StyleSheet,
+    NativeSyntheticEvent,
+    NativeScrollEvent,
+    Platform,
+    PermissionsAndroid,
+} from 'react-native';
+import GobackButton from '../../components/gobackButton';
+import {ImageBackground} from 'react-native';
+  
+import React, {useState,useEffect, Component} from 'react';
+import UpBar_Hospital from '../../components/UpBar_Hospital';
+import Map1 from '../../../assets/images/Map1.png';
+
+import { SearchBar, Icon } from 'react-native-elements';
+import Geolocation from 'react-native-geolocation-service';
+
+
+const HospitalMap = ({navigation, route}) => {
+    const [search, setSearch] = useState("");
+
+    const updateSearch = (search) => {
+        setSearch(search);
+    }
+
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLogitude] = useState("");
+
+    const GetLocation = async () => {
+        console.log('Getting location...');
+        Geolocation.getCurrentPosition(
+            position => {
+                const latitude = JSON.stringify(position.coords.latitude);
+                const longitude = JSON.stringify(position.coords.longitude);
+
+                setLatitude(latitude);
+                setLogitude(longitude);
+
+                console.log(latitude);
+                console.log(longitude);
+            },
+            error => {
+                console.log('Error getting location:', error);
+            },
+            {enableHighAccuracy:true, timeout: 15000, maximumAge: 10000 },
+        );
+    };
+
+    const CheckPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.check(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            );
+            if (granted) {
+                console.log('Permission already granted');
+                GetLocation();
+                return;
+            }
+            const result = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            );
+            if (result === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('Permission granted successfully');
+                GetLocation();
+            } else {
+                console.log('Permission denied');
+            }
+        }catch(e){
+            console.log(e);
+        }
+    };
+
+    useEffect(()=>{
+        CheckPermission();
+    },[]);
+
+    return(
+        <>
+        <UpBar_Hospital text = "병원찾기"/>
+        <GobackButton onPress={() => navigation.pop()}></GobackButton>
+        <ImageBackground source={Map1} style={styles.image}>
+            <SearchBar
+                placeholder="병원 이름 검색"
+                onChangeText={updateSearch}
+                value={search}
+                containerStyle={styles.searchBarContainer}
+                inputContainerStyle={styles.searchBarInputContainer}
+                inputStyle={styles.searchBarInput}
+            />
+        </ImageBackground>
+        </>
+    )
+}
+const styles = StyleSheet.create({
+    view: {
+        margin:10.
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        //height: '43%',
+        flex: 1,
+    },
+    searchBarContainer: {
+        backgroundColor: 'transparent',
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+        padding: 0,
+        margin: 0,
+    },
+    searchBarInputContainer: {
+        backgroundColor: '#ffffff',
+        borderRadius: 8,
+        width: '90%',
+        alignSelf: 'center',
+        height: 40,
+    },
+    searchBarInput: {
+        fontSize: 16,
+        height: 40,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: '100%',
+        height: '100%',
+      },
+});
+
+export default HospitalMap;
